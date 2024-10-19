ed6692a7c7b2b5681391538dbadfe57eff41eb13
Anders <andersjk123@gmail.com>
2024-10-19 14:01:04 +0200

    Fixed conversion to Pandas

--- read_csv.py
+++ read_csv.py
@@ -10,6 +10,11 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
 from pyspark.sql.functions import col
+from pyspark.sql.functions import substring
+from pyspark.sql.functions import concat
+from pyspark.sql.functions import lit
+from pyspark.sql.functions import dayofweek
+from pyspark.sql.functions import to_timestamp
 
 findspark.init()
 spark = SparkSession.builder.appName("pizza_sales").getOrCreate()
@@ -20,27 +25,27 @@ data = os.path.join(os.path.dirname(__file__), 'pizza_sales.csv')
 df_spark = spark.read.csv(data, header=True, inferSchema=True)
 
 # 1. How can we compare the sales across 24 hours of the day? (Bar chart)
-
 exc1_df = df_spark.select("order_date", "order_time", "quantity")
+ 
+ 
+columns = ["order_time"]
+# df_spark_tmp = spark.createDataFrame(data, columns)
+ 
+# Cast 'order_time' from string to timestamp
+df_spark_tmp = df_spark.withColumn("order_time", col("order_time").cast("string"))
+df_spark_tmp = df_spark_tmp.withColumn("order_time", substring(col("order_time"), 12, 8))
+df_spark_tmp = df_spark_tmp.withColumn(
+    'order_datetime',
+    concat(col('order_date'), lit(' '), col('order_time'))
+)
+ 
+ 
+df_spark_tmp = df_spark_tmp.withColumn(
+    'day_of_week',
+    dayofweek(to_timestamp(col('order_datetime'), 'M/d/yyyy HH:mm:ss'))  # Specify the format
+)
 
-def toWeekday(date):
-        date = str(date).split("/") # This separates the date and time
-        day, month, year = int(date[0]), int(date[1]), int(date[2])
-        day = datetime.datetime(year, month, day)
-        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-        return days[day.weekday()]
+pandas_df = df_spark_tmp.toPandas()
+print(pandas_df['day_of_week'].unique())
 
-def formatTime(time):
-    time = str(time).split(" ")
-    print(time)
-    return time[1]
-
-weekdayConv = udf(toWeekday, StringType())
-
-timeConv = udf(formatTime, StringType())
-
-exc1_df = exc1_df.withColumn("weekday", weekdayConv(exc1_df["order_date"]))
-
-exc1_df = exc1_df.withColumn("order_time", timeConv(exc1_df["order_time"]))
-
-# exc1_pandas = exc1_df.toPandas()
+pandas_df[pandas_df["day_of_week"] == 1]
