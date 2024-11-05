49a89dd58ad7a044e043dd9958997a50aeb3103a
Anders <andersjk123@gmail.com>
2024-11-05 20:13:13 +0100

    temp

---
+++ system.py
@@ -0,0 +1,16 @@
+import xgboost
+import sklearn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    df = pd.read_csv("data.csv")
+
+    model = xgboost.XGBRegressor(objective='reg:squarederror')
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42)
+
+if __name__ == "__main__":
+    main()
