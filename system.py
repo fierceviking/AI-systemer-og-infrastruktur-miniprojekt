9815d0f5502307ff2bb0c430c0a74e923eaf66c5
Anders <andersjk123@gmail.com>
2024-11-05 20:14:03 +0100

    Update system.py

--- system.py
+++ system.py
@@ -9,8 +9,8 @@ def main():
 
     model = xgboost.XGBRegressor(objective='reg:squarederror')
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42)
 
 if __name__ == "__main__":
     main()
